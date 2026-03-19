ddbb77c05d4a2f325100d299edcc1d433d13f2e0
DhruvilShah1 <dhruvilshah3383@gmail.com>
2026-03-19 10:17:09 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                resolve('resources/js/Main.jsx'),
-            ],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
+  plugins: [
+    laravel({
+      input: [
+        resolve(__dirname, 'resources/css/app.css'),  // Tailwind CSS
+        resolve(__dirname, 'resources/js/Main.jsx'),  // React entry
+      ],
+      refresh: true, // Hot reload on Blade changes
+    }),
+    react(),
+    tailwindcss(),
+  ],
 });
