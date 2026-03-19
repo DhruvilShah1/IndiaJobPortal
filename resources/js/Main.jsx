99e421df4fbbdd097f28c5282dcbdc0609e4c2d5
DhruvilShah1 <dhruvilshah3383@gmail.com>
2026-03-19 10:13:38 +0530

    update

--- resources/js/Main.jsx
+++ resources/js/Main.jsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthContext, AuthProvider } from "./src/component/LoginRegisterComponent/AuthContext";
+import { AuthProvider } from "./src/component/LoginRegisterComponent/AuthContext";
 
 
 const Main = () => {
--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
-import react from 'react';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                resolve(__dirname, 'resources/js/src/main.jsx'),
+                resolve('resources/js/Main.jsx'),
             ],
             refresh: true,
         }),
         tailwindcss(),
-        react(),
     ],
     server: {
         watch: {
